e01290d6f0b4634b59182d331226a7eb2cb1bbc7
Karandeep-56 <karandeep.singh9121@gmail.com>
2025-06-28 22:57:59 -0700

    order details of customers

--- sql_interview_questions.sql
+++ sql_interview_questions.sql
@@ -45,3 +45,24 @@ JOIN dept_avg d ON e.department = d.department
 order by  e.department
 
 
+----Find the details of each customer regardless of whether the customer
+----made an order. Output the customer's first name, last name, and the 
+----city along with the order details.
+SELECT c.first_name,
+c.last_name,
+c.city, 
+o.order_details 
+from customers c
+LEFT JOIN orders o ON o.cust_id = c.id
+order by c.first_name, o.order_details
+
+
+
+
+
+
+
+
+
+Sort records based on the customer's first name and the order details in ascending order.
+
